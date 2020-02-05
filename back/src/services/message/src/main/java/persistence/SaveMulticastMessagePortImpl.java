d30faa3c9e71640a7b283a195e6ebd0bb8882c6e
ilyo <ilyassnasr@sqli.com>
2020-02-05 16:24:38 +0100

    Add SaveMulticastMessagePort Implementation

---
+++ back/src/services/message/src/main/java/persistence/SaveMulticastMessagePortImpl.java
@@ -0,0 +1,14 @@
+package persistence;
+
+import domain.MulticastMessage;
+import ports.out.SaveMulticastMessagePort;
+
+public class SaveMulticastMessagePortImpl implements SaveMulticastMessagePort {
+
+    @Override
+    public void saveMulticastMessage(MulticastMessage message) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
